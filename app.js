bdafe9e561e650c279d48b7d340ddd1c34f73421
Paul Salmon <salmon880@hotmail.com>
2016-04-01 11:31:54 +1100

    bug sorted

--- app.js
+++ app.js
@@ -23,10 +23,15 @@ var rounds = 0;
 var xWins = 0;
 var oWins = 0;
 
+var won = false;
 
-winCount(); //needed so that scores (0-0) display when pages loads.
+
+// winCount(); //needed so that scores (0-0) display when pages loads.
 //checks and increments the turns and changes the background
 board.addEventListener('click', function(event) {
+    if (won) {
+      return;
+    }
         var cell = event.target;
     if(cell.className === "box" && cell.innerHTML !== "X" && cell.innerHTML !== "O" && xWins < 5 && oWins < 5){
       console.log(xWins + " " + oWins)
@@ -48,116 +53,138 @@ board.addEventListener('click', function(event) {
 
 
 board.addEventListener('click', function(event) {
+  if (won) {
+    return;
+  }
         var cell = event.target;
-        // console.log(event);
 
+
+        // console.log(event);
         cells[cell.id] = cell.innerHTML;
         // console.log(event.target.id)
                       // console.log(cells)
-if ( xWins < 5 || oWins < 5) {
+    if ( xWins < 5 && oWins < 5) {
+      console.log('wins are less than five')
       for(var prop in cells) {
           if(cells.hasOwnProperty(prop)) {
                 if(cells.tm === "X" && cells.tl === "X" && cells.tr === "X") {
                   document.getElementById('winner').innerHTML= 'X Wins round ' + (rounds +1)
                   xWins++;
                   rounds++;
+                  console.log('its here');
                   winCount();
                   return;
                 }else if (cells.mm === "X" && cells.ml === "X" && cells.mr === "X") {
                   document.getElementById('winner').innerHTML='X Wins round ' + (rounds +1)
                   xWins++;
                   rounds++;
+                  console.log('its here');
                   winCount();
                   return;
                 } else if (cells.bm === "X" && cells.bl === "X" && cells.br === "X"){
                   document.getElementById('winner').innerHTML='X Wins round ' + (rounds +1)
                   xWins++;
                   rounds++;
+                  console.log('its here');
                   winCount();
                     return;
                 }else if (cells.bl === "X" && cells.ml === "X" && cells.tl === "X"){
                   document.getElementById('winner').innerHTML='X Wins round ' + (rounds +1)
                   xWins++
                   rounds++;
+                  console.log('its here');
                   winCount();
                     return;
                 }else if (cells.bm === "X" && cells.mm === "X" && cells.tm === "X"){
                   document.getElementById('winner').innerHTML='X Wins  round ' + (rounds +1)
                   xWins++
                   rounds++;
+                  console.log('its here');
                   winCount();
                     return;
                 }else if (cells.br === "X" && cells.mr === "X" && cells.tr === "X"){
                   document.getElementById('winner').innerHTML='X Wins round ' + (rounds +1)
                   xWins++
                   rounds++;
+                  console.log('its here');
                   winCount();
                     return;
                 }else if (cells.bl === "X" && cells.mm === "X" && cells.tr === "X"){
                   document.getElementById('winner').innerHTML='X Wins round ' +(rounds +1)
                   xWins++
                   rounds++;
+                  console.log('its here');
                   winCount();
                     return;
                 }else if (cells.tl === "X" && cells.mm === "X" && cells.br === "X"){
                   document.getElementById('winner').innerHTML='X Wins round ' + (rounds +1)
                   xWins++
                   rounds++;
+                  console.log('its here');
                   winCount();
                     return;
                 }else if (cells.bm === "O" && cells.bl === "O" && cells.br === "O"){
                   document.getElementById('winner').innerHTML='O Wins round ' + (rounds +1)
                   oWins++
                   rounds++;
+                  console.log('its here');
                   winCount();
                     return;
                 }else if (cells.bl === "O" && cells.ml === "O" && cells.tl === "O"){
                   document.getElementById('winner').innerHTML='O Wins round ' + (rounds +1)
                   oWins++
                   rounds++;
+                  console.log('its here');
                   winCount();
                     return;
                 }else if (cells.bm === "O" && cells.mm === "O" && cells.tm === "O"){
                   document.getElementById('winner').innerHTML='O Wins round ' + (rounds +1)
                   oWins++
                   rounds++;
+                  console.log('its here');
                   winCount();
                     return;
                 }else if (cells.br === "O" && cells.mr === "O" && cells.tr === "O"){
                   document.getElementById('winner').innerHTML='O Wins round ' + (rounds +1)
                   oWins++
                   rounds++;
+                  console.log('its here');
                   winCount();
                     return;
                 }else if (cells.br === "O" && cells.mm === "O" && cells.tl === "O"){
                   document.getElementById('winner').innerHTML='O Wins round ' + (rounds +1)
                   oWins++
                   rounds++;
+                  console.log('its here');
                   winCount();
                     return;
                 }else if (cells.tl === "O" && cells.mm === "O" && cells.br === "O"){
                   document.getElementById('winner').innerHTML='O Wins round ' + (rounds +1)
                   oWins++
                   rounds++;
+                  console.log('its here');
                   winCount();
                     return;
                 }else if (cells.bl === "O" && cells.mm === "O" && cells.tr === "O"){
                   document.getElementById('winner').innerHTML='O Wins round ' + (rounds +1)
                   oWins++
                   rounds++;
+                  console.log('its here');
                   winCount();
                     return;
                 }else if (cells.mr === "O" && cells.mm === "O" && cells.ml === "O"){
                   document.getElementById('winner').innerHTML='O Wins round ' + (rounds +1)
                   oWins++
                   rounds++;
+                  console.log('its here');
                   winCount();
                     return;
                 }
               }
-            }} else {console.log("should stop")}
+            }}
+            else {console.log("should stop")}
   checkDraw();
-  winCount();
+  // winCount();
 })
 
 function checkDraw() {
@@ -175,6 +202,8 @@ function checkDraw() {
 }
 
 function winCount(){
+  console.log('someone won')
+  won = true;
   if(xWins === 5) { document.getElementById('winner').innerHTML='X Is The CHAMP!'
     document.getElementById('xWinCounter').innerHTML = "X : " + xWins + " WINS.";
     document.getElementById('oWinCounter').innerHTML = "O : " + oWins + " WINS.";
@@ -204,6 +233,7 @@ for(i=0;i<9;i++){
   cells.bm ='this is bm';
   cells.br ='this is br';
   playerTurn = 0;
+  won = false;
   document.getElementById('playerChoice').innerHTML = 'X To Move!';
 }
 })
